2f385d3e2babc0642a004b4cd637ab355f99425d
Admin <sandesh@blockchaininternationale.com>
2022-12-09 15:08:56 +0530

    second commit

--- lib/screens/grid_screen.dart
+++ lib/screens/grid_screen.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:searchfield/searchfield.dart';
-import '../models/alphabet_model.dart';
-import '../models/grid_model.dart';
+import 'package:mobilogic_test/models/alpha_model.dart';
+import 'package:mobilogic_test/models/grid_model.dart';
 
 class GridScreen extends StatefulWidget {
   final GridModel gridModel;
-
   const GridScreen({Key? key, required this.gridModel}) : super(key: key);
 
   @override
@@ -13,109 +11,80 @@ class GridScreen extends StatefulWidget {
 }
 
 class _GridScreenState extends State<GridScreen> {
-  final TextEditingController _searchController = TextEditingController();
-
-  List<AlphabetModel> _alphabets = [];
-  List<String> selected = [];
+  final TextEditingController _controller = TextEditingController();
+  List<AlphaModel> _str = [];
 
   @override
   void initState() {
-    super.initState();
     for (String element in (widget.gridModel.alphabets?.split("") ?? [])) {
-      _alphabets.add(AlphabetModel(char: element, isSelected: false));
+      _str.add(AlphaModel(char: element, isSelected: false));
     }
+    String text1 = '';
+    _controller.addListener(
+          () {
+        text1 = _controller.text;
+        var searched = _str.map((e) {
+          if (text1.contains(e.char)) {
+            e.isSelected = true;
+          } else {
+            e.isSelected = false;
+          }
+          return e;
+        }).toList();
+        setState(() => _str = searched);
+      },
+    );
+
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                SizedBox(
-                  child: GridView.count(
-                    crossAxisCount: widget.gridModel.columns ?? 0,
-                    shrinkWrap: true,
-                    mainAxisSpacing: 10,
-                    crossAxisSpacing: 10,
-                    children: _alphabets.map(_validator).toList(),
-                  ),
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: Column(
+            children: [
+              Expanded(
+                child: GridView.count(
+                  crossAxisCount: widget.gridModel.columns ?? 0,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  children: _str.map(_validator).toList(),
                 ),
-                const SizedBox(
-                  height: 30,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.teal[500],
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  margin: const EdgeInsets.symmetric(vertical: 5),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 15),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text(
-                          'Enter Characters',
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.black,
-                          ),
-                        ),
-                        SearchField(
-                          suggestions: const [],
-                          controller: _searchController,
-                        ),
-                      ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                color: Colors.teal[500],
+                child: TextField(
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Search",
+                    hintStyle: TextStyle(
+                      color: Colors.white30,
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      String text1 = _searchController.toString();
-                      var searched = _alphabets.map((e) {
-                        if (text1.contains(e.char)) {
-                          e.isSelected = true;
-                          selected.add(e.char);
-                        } else {
-                          e.isSelected = false;
-                          selected.remove(e.char);
-                        }
-                        return e;
-                      });
-                      setState(() => _alphabets = searched.toList());
-                    });
-                  },
-                  child: Container(
-                    width: double.infinity,
-                    height: 70,
-                    decoration: BoxDecoration(
-                      color: Colors.teal[500],
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    margin: const EdgeInsets.symmetric(vertical: 5),
-                    child: const Center(
-                      child: Text('Search Text'),
-                    ),
+                  controller: _controller,
+                  cursorColor: Colors.black,
+                  keyboardType: TextInputType.text,
+                  style: const TextStyle(
+                    color: Colors.black,
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 
-  Widget _validator(AlphabetModel alphabetModel) {
+  Widget _validator(AlphaModel alphabetModel) {
     return Container(
       color: alphabetModel.isSelected
           ? Colors.teal
@@ -134,4 +103,5 @@ class _GridScreenState extends State<GridScreen> {
       ),
     );
   }
+
 }
